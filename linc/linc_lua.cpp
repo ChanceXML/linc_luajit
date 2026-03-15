3b325a2e16e12184c8b83b926ea9e5cedf4ec458
Poll-st <invtjosesito@gmail.com>
2026-03-15 12:23:13 -0700

    Fixed linc_lua.cpp

--- linc/linc_lua.cpp
+++ linc/linc_lua.cpp
@@ -1,299 +1,272 @@
 #include <hxcpp.h>
 #include <hx/CFFI.h>
+#include <sstream>
 
 #include "./linc_lua.h"
 #include "../lib/lua/src/lua.hpp"
 
 namespace linc {
 
-    namespace lua {
+namespace lua {
 
-        ::String version(){
+::String version(){
+    return ::String(LUA_VERSION);
+}
 
-        	return ::String(LUA_VERSION);
+::String versionJIT(){
+    return ::String(LUAJIT_VERSION);
+}
 
-        }
+::String tostring(lua_State *l, int v){
+    const char* s = lua_tostring(l, v);
+    return s ? ::String(s) : ::String("");
+}
 
-        ::String versionJIT(){
-        	return ::String(LUAJIT_VERSION);
-        }
+::String tolstring(lua_State *l, int v, size_t *len){
+    const char* s = lua_tolstring(l, v, len);
+    return s ? ::String(s) : ::String("");
+}
 
-        ::String tostring(lua_State *l, int v){
+::cpp::Function<int(lua_State*)> tocfunction(lua_State* l, int i) {
+    return (::cpp::Function<int(lua_State*)>) lua_tocfunction(l, i);
+}
 
-            return ::String(lua_tostring(l, v));
+void pushcclosure(lua_State* l, ::cpp::Function<int(lua_State*)> fn, int n) {
+    lua_pushcclosure(l, (lua_CFunction)fn, n);
+}
 
-        }
+void pushcfunction(lua_State* l, ::cpp::Function<int(lua_State*)> fn) {
+    lua_pushcfunction(l, (lua_CFunction)fn);
+}
 
-        ::String tolstring(lua_State *l, int v, size_t *len){
+::String _typename(lua_State *l, int v){
+    const char* s = lua_typename(l, v);
+    return s ? ::String(s) : ::String("");
+}
 
-            return ::String(lua_tolstring(l, v, len));
+/* IMPORTANT for Android */
+void openlibs(lua_State* L)
+{
+    luaL_openlibs(L);
+}
 
-        }
+int getstack(lua_State *L, int level, Dynamic ar){
 
-        ::cpp::Function<int(lua_State*)> tocfunction(lua_State* l, int i) {
-            return (::cpp::Function<int(lua_State*)>) lua_tocfunction(l, i);
-        }
+    lua_Debug dbg;
+    int ret = lua_getstack(L, level, &dbg);
 
-        void pushcclosure(lua_State* l, ::cpp::Function<int(lua_State*)> fn, int n) {
-            lua_pushcclosure(l, (lua_CFunction)fn, n);
-        }
+    ar->__FieldRef(HX_CSTRING("i_ci")) = (int)dbg.i_ci;
 
-        void pushcfunction(lua_State* l, ::cpp::Function<int(lua_State*)> fn) {
-            lua_pushcfunction(l, (lua_CFunction)fn);
-        }
+    return ret;
+}
 
-        ::String _typename(lua_State *l, int v){
+int getinfo(lua_State *L, const char *what, Dynamic ar){
 
-            return ::String(lua_typename(l, v));
+    lua_Debug dbg;
+    dbg.i_ci = ar->__FieldRef(HX_CSTRING("i_ci"));
 
-        }
+    int ret = lua_getinfo(L, what, &dbg);
 
-        int getstack(lua_State *L, int level, Dynamic ar){
+    if (strchr(what, 'S')) {
 
-            lua_Debug dbg;
+        if (dbg.source)
+            ar->__FieldRef(HX_CSTRING("source")) = ::String(dbg.source);
 
-            int ret = lua_getstack(L, level, &dbg);
+        if (dbg.short_src)
+            ar->__FieldRef(HX_CSTRING("short_src")) = ::String(dbg.short_src);
 
-            ar->__FieldRef(HX_CSTRING("i_ci")) = (int)dbg.i_ci;
+        if (dbg.linedefined != 0)
+            ar->__FieldRef(HX_CSTRING("linedefined")) = (int)dbg.linedefined;
 
-            return ret;
+        if (dbg.lastlinedefined != 0)
+            ar->__FieldRef(HX_CSTRING("lastlinedefined")) = (int)dbg.lastlinedefined;
 
-        }
+        if (dbg.what)
+            ar->__FieldRef(HX_CSTRING("what")) = ::String(dbg.what);
+    }
 
-        int getinfo(lua_State *L, const char *what, Dynamic ar){
+    if (strchr(what, 'n')) {
 
-            lua_Debug dbg;
+        if (dbg.name)
+            ar->__FieldRef(HX_CSTRING("name")) = ::String(dbg.name);
 
-            dbg.i_ci = ar->__FieldRef(HX_CSTRING("i_ci"));
+        if (dbg.namewhat)
+            ar->__FieldRef(HX_CSTRING("namewhat")) = ::String(dbg.namewhat);
+    }
 
-            int ret = lua_getinfo(L, what, &dbg);
+    if (strchr(what, 'l')) {
 
-            if (strchr(what, 'S')) {
+        if (dbg.currentline != 0)
+            ar->__FieldRef(HX_CSTRING("currentline")) = (int)dbg.currentline;
+    }
 
-                if (dbg.source != NULL) {
-                    ar->__FieldRef(HX_CSTRING("source")) = ::String(dbg.source);
-                }
+    if (strchr(what, 'u')) {
 
-                if (dbg.short_src != NULL) {
-                    ar->__FieldRef(HX_CSTRING("short_src")) = ::String(dbg.short_src);
-                }
+        if (dbg.nups != 0)
+            ar->__FieldRef(HX_CSTRING("nups")) = (int)dbg.nups;
+    }
 
-                if (dbg.linedefined != NULL) {
-                    ar->__FieldRef(HX_CSTRING("linedefined")) = (int)dbg.linedefined;
-                }
+    return ret;
+}
 
-                if (dbg.lastlinedefined != NULL) {
-                    ar->__FieldRef(HX_CSTRING("lastlinedefined")) = (int)dbg.lastlinedefined;
-                }
+} // lua
 
-                if (dbg.what != NULL) {
-                    ar->__FieldRef(HX_CSTRING("what")) = ::String(dbg.what);
-                }
 
-            }
+namespace lual {
 
-            if (strchr(what, 'n')) {
-                if (dbg.name != NULL) {
-                    ar->__FieldRef(HX_CSTRING("name")) = ::String(dbg.name);
-                }
+::String checklstring(lua_State *l, int numArg, size_t *len){
+    const char* s = luaL_checklstring(l, numArg, len);
+    return s ? ::String(s) : ::String("");
+}
 
-                if (dbg.namewhat != NULL) {
-                    ar->__FieldRef(HX_CSTRING("namewhat")) = ::String(dbg.namewhat);
-                }
-            }
+::String optlstring(lua_State *l, int numArg, const char *def, size_t *len){
+    const char* s = luaL_optlstring(l, numArg, def, len);
+    return s ? ::String(s) : ::String("");
+}
 
-            if (strchr(what, 'l')) {
-                if (dbg.currentline != NULL) {
-                    ar->__FieldRef(HX_CSTRING("currentline")) = (int)dbg.currentline;
-                }
-            }
+::String prepbuffer(luaL_Buffer *B){
+    return ::String(luaL_prepbuffer(B));
+}
 
-            if (strchr(what, 'u')) {
-                if (dbg.nups != NULL) {
-                    ar->__FieldRef(HX_CSTRING("nups")) = (int)dbg.nups;
-                }
-            }
+::String gsub(lua_State *l, const char *s, const char *p, const char *r){
+    return ::String(luaL_gsub(l, s, p, r));
+}
 
-            return ret;
+::String findtable(lua_State *L, int idx, const char *fname, int szhint){
+    return ::String(luaL_findtable(L, idx, fname, szhint));
+}
 
-        }
+::String checkstring(lua_State *L, int n){
+    const char* s = luaL_checkstring(L, n);
+    return s ? ::String(s) : ::String("");
+}
 
-    } //lua
+::String optstring(lua_State *L, int n, const char *d){
+    const char* s = luaL_optstring(L, n, d);
+    return s ? ::String(s) : ::String("");
+}
 
-    namespace lual {
+void error(lua_State *L, const char* fmt) {
+    luaL_error(L,fmt,"");
+}
 
-        ::String checklstring(lua_State *l, int numArg, size_t *len){
+::String ltypename(lua_State *L, int idx){
+    const char* s = luaL_typename(L, idx);
+    return s ? ::String(s) : ::String("");
+}
 
-            return ::String(luaL_checklstring(l, numArg, len));
+} // lual
 
-        }
 
-        ::String optlstring(lua_State *l, int numArg, const char *def, size_t *len){
+namespace helpers {
 
-            return ::String(luaL_optlstring(l, numArg, def, len));
+static int onError(lua_State *L) {
 
-        }
+    const char *msg = lua_tostring(L, 1);
 
-        ::String prepbuffer(luaL_Buffer *B){
+    if (msg)
+        luaL_traceback(L, L, msg, 1);
+    else if (!lua_isnoneornil(L, 1)) {
+        if (!luaL_callmeta(L, 1, "__tostring"))
+            lua_pushliteral(L, "(no error message)");
+    }
 
-            return ::String(luaL_prepbuffer(B));
+    return 1;
+}
 
-        }
+int setErrorHandler(lua_State *L){
+    lua_pushcfunction(L, onError);
+    return 1;
+}
 
-        ::String gsub(lua_State *l, const char *s, const char *p, const char *r){
+static HxTraceFN print_fn = 0;
 
-            return ::String(luaL_gsub(l, s, p, r));
+static int hx_trace(lua_State* L) {
 
-        }
+    std::stringstream buffer;
 
-        ::String findtable(lua_State *L, int idx, const char *fname, int szhint){
+    int n = lua_gettop(L);
 
-            return ::String(luaL_findtable(L, idx, fname, szhint));
+    lua_getglobal(L,"tostring");
 
-        }
+    for (int i = 1; i <= n; ++i){
 
-        ::String checkstring(lua_State *L, int n){
+        const char* s = NULL;
+        size_t l = 0;
 
-            return ::String(luaL_checkstring(L, n));
+        lua_pushvalue(L,-1);
+        lua_pushvalue(L,i);
 
-        }
+        lua_call(L,1,1);
 
-        ::String optstring(lua_State *L, int n, const char *d){
+        s = lua_tolstring(L,-1, &l);
 
-            return ::String(luaL_optstring(L, n, d));
+        if (!s)
+            return luaL_error(L,"tostring must return string");
 
-        }
+        if (i > 1)
+            buffer << "\t";
 
-        void error(lua_State *L, const char* fmt) {
-            luaL_error(L,fmt,"");
-        }
+        buffer << s;
 
-        ::String ltypename(lua_State *L, int idx){
+        lua_pop(L,1);
+    }
 
-            return ::String(luaL_typename(L, idx));
+    if(print_fn)
+        print_fn(::String(buffer.str().c_str()));
 
-        }
+    return 0;
+}
 
-    } //lual
+static const struct luaL_Reg printlib [] = {
 
-    namespace helpers {
+    {"print", hx_trace},
+    {NULL, NULL}
 
-        static int onError(lua_State *L) {
+};
 
-            const char *msg = lua_tostring(L, 1);
+void register_hxtrace_func(HxTraceFN fn){
+    print_fn = fn;
+}
 
-            if (msg)
-                luaL_traceback(L, L, msg, 1);
-            else if (!lua_isnoneornil(L, 1)) {  /* is there an error object? */
-                if (!luaL_callmeta(L, 1, "__tostring"))  /* try its 'tostring' metamethod */
-                    lua_pushliteral(L, "(no error message)");
-            }
+void register_hxtrace_lib(lua_State* L){
 
-            return 1;
+    lua_getglobal(L, "_G");
+    luaL_register(L, NULL, printlib);
+    lua_pop(L, 1);
 
-        }
+}
 
-        int setErrorHandler(lua_State *L){
+} // helpers
 
-            lua_pushcfunction(L, onError);
-            return 1;
 
-        }
+namespace callbacks {
 
+static luaCallbackFN event_fn = 0;
 
-        // haxe trace function
+static int luaCallback(lua_State *L){
+    return event_fn(L, ::String(lua_tostring(L, lua_upvalueindex(1))));
+}
 
-        static HxTraceFN print_fn = 0;
-        static int hx_trace(lua_State* L) {
+void set_callbacks_function(luaCallbackFN fn){
+    event_fn = fn;
+}
 
-            std::stringstream buffer;
-            int n = lua_gettop(L);  /* number of arguments */
+void add_callback_function(lua_State *L, const char *name) {
 
-            lua_getglobal(L,"tostring");
+    lua_pushstring(L, name);
+    lua_pushcclosure(L, luaCallback, 1);
+    lua_setglobal(L, name);
 
-            for ( int i = 1;  i <= n;  ++i ){
-                const char* s = NULL;
-                size_t      l = 0;
+}
 
-                lua_pushvalue(L,-1);    /* function to be called */
-                lua_pushvalue(L,i); /* value to print */
-                lua_call(L,1,1);
+void remove_callback_function(lua_State *L, const char *name){
 
-                s = lua_tolstring(L,-1, &l); /* get result */
-                if ( s == NULL ){
-                    return luaL_error(L,LUA_QL("tostring") " must return a string to " LUA_QL("print"));
-                }
+    lua_pushnil(L);
+    lua_setglobal(L, name);
 
-                if ( i>1 ){
-                    buffer << "\t";
-                }
+}
 
-                buffer << s;
+} // callbacks
 
-                lua_pop(L,1);   /* pop result */
-            }
-
-            // std::cout << buffer.str(); // c++ out
-            print_fn(::String(buffer.str().c_str())); // hx out
-
-            return 0;
-
-        }
-
-        static const struct luaL_Reg printlib [] = {
-
-            {"print", hx_trace},
-            {NULL, NULL} /* end of array */
-
-        };
-
-        void register_hxtrace_func(HxTraceFN fn){
-
-            print_fn = fn;
-
-        }
-
-        void register_hxtrace_lib(lua_State* L){
-
-            lua_getglobal(L, "_G");
-            luaL_register(L, NULL, printlib);
-            lua_pop(L, 1);
-
-        }
-
-    } //helpers
-
-    namespace callbacks {
-
-        static luaCallbackFN event_fn = 0;
-        static int luaCallback(lua_State *L){
-
-            return event_fn(L, ::String(lua_tostring(L, lua_upvalueindex(1))));
-
-        }
-
-        void set_callbacks_function(luaCallbackFN fn){
-
-            event_fn = fn;
-
-        }
-
-        void add_callback_function(lua_State *L, const char *name) {
-
-            lua_pushstring(L, name);
-            lua_pushcclosure(L, luaCallback, 1);
-            lua_setglobal(L, name);
-
-        }
-
-        void remove_callback_function(lua_State *L, const char *name){
-
-            lua_pushnil(L);
-            lua_setglobal(L, name);
-
-        }
-
-    } //callbacks
-
-
-} //linc
+} // linc
