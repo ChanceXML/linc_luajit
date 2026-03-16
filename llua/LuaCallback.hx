d4410c15978ed3f0e27a06f8146a3ef8c154a62c
cubed <214192747+Poll-st@users.noreply.github.com>
2026-03-16 11:05:04 -0700

    idk

--- llua/LuaCallback.hx
+++ llua/LuaCallback.hx
@@ -11,38 +11,32 @@ class LuaCallback {
     /** Pointer to Lua function reserved for temporary use */
     public var ref(default, null):Int;
 
-    public function new(lua:State, ref:Int) {
-        this.l = lua;
+    public function new(lua:Dynamic, ref:Int) {
+        this.l = cast lua;
         this.ref = ref;
     }
 
     /** Runs this Lua function once, with the given arguments. */
     public function call(args:Array<Dynamic> = null) {
 
-        // Push the function from registry onto the stack
         Lua.rawgeti(l, Lua.LUA_REGISTRYINDEX, ref);
 
-        // Check if it's actually a function
         if (!Lua.isfunction(l, -1)) {
             Lua.pop(l, 1);
             return;
         }
 
-        // Convert arguments to Lua
         if (args == null) args = [];
         for (arg in args) Convert.toLua(l, arg);
 
-        // Call the function
         var status:Int = Lua.pcall(l, args.length, 0, 0);
 
         if (status != Lua.LUA_OK) {
             var err:String = null;
 
-            // Only read error if stack top is not nil
             if (!Lua.isNilBool(l, -1)) err = Lua.tostring(l, -1);
             Lua.pop(l, 1);
 
-            // Fallback error messages
             if (err == null || err == "") {
                 switch(status) {
                     case Lua.LUA_ERRRUN: err = "Runtime Error";
