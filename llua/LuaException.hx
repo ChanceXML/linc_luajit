18c1dba1b7adba798b6fe8a8d63e3583bb09e053
Poll-st <invtjosesito@gmail.com>
2026-03-15 20:05:52 -0700

    Update LuaException.hx

--- llua/LuaException.hx
+++ llua/LuaException.hx
@@ -1,27 +1,27 @@
 package llua;
+
 import llua.State;
 import haxe.Exception;
 
 class LuaException extends Exception {
 	public var error_code:Int = 0;
     
-	public function new(?message:String, ?previous:Exception, ?code:Int = 0, luaState:State) {
-		error_code = code;
-		if(message == null) {
-			message = switch(code) {
-				case Lua.LUA_ERRRUN: (luaState == null ? "Lua Runtime Error: UNKNOWN ERROR?" : Lua.tostring(luaState, -1));
-				case Lua.LUA_ERRMEM: "luavm ran out of memory";
-				case Lua.LUA_ERRERR: "LUA_ERRERR";
-				default: "Lua Error:" + code;
-			};
-		}
+	public function new(message:String, ?previous:Exception, ?code:Int = 0) {
+		error_code = code != null ? code : 0;
 		super(message, previous);
 	}
     
 	public static function ifErrorThrow(l:State, status:Int) {
 		if(status == 0) return;
         
-		var exception = new LuaException(null, null, status, l);
+		var errorMsg:String = switch(status) {
+			case Lua.LUA_ERRRUN: (l == null ? "Lua Runtime Error: UNKNOWN ERROR?" : Lua.tostring(l, -1));
+			case Lua.LUA_ERRMEM: "luavm ran out of memory";
+			case Lua.LUA_ERRERR: "LUA_ERRERR";
+			default: "Lua Error: " + status;
+		};
+        
+		var exception = new LuaException(errorMsg, null, status);
         
 		#if android
 		trace("LUA EXCEPTION CAUGHT: " + exception.message);
