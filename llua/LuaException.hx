9940da4387a81396efa95a03e18772ad631609eb
Poll-st <invtjosesito@gmail.com>
2026-03-15 19:47:47 -0700

    Update LuaException.hx

--- llua/LuaException.hx
+++ llua/LuaException.hx
@@ -1,51 +1,38 @@
 package llua;
 
 import llua.State;
-import llua.Convert;
+import llua.Lua;
+import haxe.Exception;
 
-class LuaCallback {
+class LuaException extends Exception {
+    public var error_code:Int = 0;
 
-    private var l:State;
-    public var ref(default, null):Int;
+    public function new(?message:String, ?previous:Exception, ?code:Int = 0, ?luaState:State) {
+        error_code = code;
 
-    public function new(lua:State, ref:Int) {
-        this.l = lua;
-        this.ref = ref;
-    }
-
-    public function call(args:Array<Dynamic> = null) {
-
-        Lua.rawgeti(l, Lua.LUA_REGISTRYINDEX, ref);
-
-        if (!Lua.isfunction(l, -1)) {
-            Lua.pop(l, 1);
-            return;
+        if (message == null) {
+            message = switch(code) {
+                case Lua.LUA_ERRRUN:
+                    if(luaState == null) {
+                        "Lua Runtime Error";
+                    } else {
+                        var err = Lua.tostring(luaState, -1);
+                        err == null ? "Lua Runtime Error" : err;
+                    }
+                case Lua.LUA_ERRMEM:
+                    "Lua VM ran out of memory";
+                case Lua.LUA_ERRERR:
+                    "Lua Error while handling another error";
+                default:
+                    "Lua Error: " + code;
+            };
         }
 
-        if (args == null) args = [];
-        for (arg in args) Convert.toLua(l, arg);
-
-        var status:Int = Lua.pcall(l, args.length, 0, 0);
-
-        if (status != Lua.LUA_OK) {
-            var err:String = null;
-            if (!Lua.isNilBool(l, -1)) err = Lua.tostring(l, -1);
-            Lua.pop(l, 1);
-
-            if (err == null || err == "") {
-                switch(status) {
-                    case Lua.LUA_ERRRUN: err = "Runtime Error";
-                    case Lua.LUA_ERRMEM: err = "Memory Allocation Error";
-                    case Lua.LUA_ERRERR: err = "Critical Error";
-                    default: err = "Unknown Error";
-                }
-            }
-
-            trace("Error on callback: " + err);
-        }
+        super(message, previous);
     }
 
-    public function dispose() {
-        LuaL.unref(l, Lua.LUA_REGISTRYINDEX, ref);
+    public static function ifErrorThrow(l:State, status:Int) {
+        if (status == Lua.LUA_OK) return;
+        throw new LuaException(null, null, status, l);
     }
 }
