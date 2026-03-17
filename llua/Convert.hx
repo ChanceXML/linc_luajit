a0b7f33e0c431c6f91ce6ae40bea8a739883c92f
cubed <214192747+Poll-st@users.noreply.github.com>
2026-03-16 19:21:19 -0700

    made some tweaks to make this stable

--- llua/Convert.hx
+++ llua/Convert.hx
@@ -19,117 +19,113 @@ public static var functionReferences:ObjectMap<Dynamic, Array<Dynamic>> = new Ob
 }
 
 public static function toLua(l:State, val:Any):Bool {
-
-	switch (Type.typeof(val)) {
-
-		case Type.ValueType.TNull:
-			Lua.pushnil(l);
-
-		case Type.ValueType.TBool:
-			Lua.pushboolean(l, val);
-
-		case Type.ValueType.TInt:
-			Lua.pushinteger(l, cast(val, Int));
-
-		case Type.ValueType.TFloat:
-			Lua.pushnumber(l, val);
-
-		case Type.ValueType.TClass(String):
-			Lua.pushstring(l, cast(val, String));
-
-		case Type.ValueType.TClass(Array):
-			arrayToLua(l, val);
-
-		case Type.ValueType.TClass(haxe.ds.StringMap) | Type.ValueType.TClass(haxe.ds.ObjectMap):
-			mapToLua(l, val);
-
-		case Type.ValueType.TObject:
-			anonToLua(l, val);
-
-		default:
-			if(enableUnsupportedTraces)
-				trace('Unsupported Haxe value: $val type ${Type.typeof(val)}');
-			return false;
+	
+	if (val == null) {
+		Lua.pushnil(l);
+		return true;
 	}
 
-	return true;
+	if (Std.isOfType(val, String)) {
+		Lua.pushstring(l, cast(val, String));
+		return true;
+	}
+
+	if (Std.isOfType(val, Bool)) {
+		Lua.pushboolean(l, cast(val, Bool));
+		return true;
+	}
+
+	if (Std.isOfType(val, Int)) {
+		Lua.pushinteger(l, cast(val, Int));
+		return true;
+	}
+
+	if (Std.isOfType(val, Float)) {
+		Lua.pushnumber(l, cast(val, Float));
+		return true;
+	}
+
+	if (Std.isOfType(val, Array)) {
+		arrayToLua(l, val);
+		return true;
+	}
+
+	if (Std.isOfType(val, haxe.ds.StringMap) || Std.isOfType(val, haxe.ds.ObjectMap)) {
+		mapToLua(l, val);
+		return true;
+	}
+
+	if (Type.typeof(val) == Type.ValueType.TObject) {
+		anonToLua(l, val);
+		return true;
+	}
+
+	if (enableUnsupportedTraces)
+		trace('Unsupported Haxe value: $val type ${Type.typeof(val)}');
+		
+	return false;
 }
 
-public static function callback_handler(cbf:Dynamic,l:State,?object:Dynamic):Int {
-
-	try{
-
+public static function callback_handler(cbf:Dynamic, l:State, ?object:Dynamic):Int {
+	try {
 		final nparams:Int = Lua.gettop(l);
 
 		if(cbf == null) return 0;
 
-		final ret:Dynamic = Reflect.callMethod(object,cbf,[for (i in 0...nparams) fromLua(l, i + 1)]);
+		final ret:Dynamic = Reflect.callMethod(object, cbf, [for (i in 0...nparams) fromLua(l, i + 1)]);
 
 		if(ret != null){
 			toLua(l, ret);
 			return 1;
 		}
 
-	}catch(e){
-		trace('$e');
-		throw(e);
+	} catch(e:Dynamic) {
+		trace('Lua Callback Error: $e');
 	}
 
 	return 0;
 }
 
 @:keep public static inline function arrayToLua(l:State, arr:Array<Any>) {
-
 	Lua.createtable(l, arr.length, 0);
 
 	for (i => v in arr) {
-
 		Lua.pushnumber(l, i + 1);
 		toLua(l, v);
 		Lua.settable(l, -3);
-
 	}
 }
 
 @:keep static inline function mapToLua(l:State, res:Map<String,Dynamic>) {
-
 	Lua.createtable(l, 0, 0);
 
 	for (index => val in res){
-
 		Lua.pushstring(l, Std.string(index));
 		toLua(l, val);
 		Lua.settable(l, -3);
-
 	}
 }
 
 @:keep static inline function anonToLua(l:State, res:Any) {
-
 	Lua.createtable(l, 0, 0);
 
 	for (n in Reflect.fields(res)){
-
 		Lua.pushstring(l, n);
 		toLua(l, Reflect.field(res, n));
 		Lua.settable(l, -3);
-
 	}
 }
 
 @:keep public static inline function setGlobal(l:State, index:String, value:Dynamic) {
-
-	toLua(l, value);
-	Lua.setfield(l, Lua.LUA_GLOBALSINDEX, index);
-
+	if (toLua(l, value)) {
+		Lua.setfield(l, Lua.LUA_GLOBALSINDEX, index);
+	}
 }
 
 public static function fromLua(l:State, v:Int):Any {
-
 	final luaType = Lua.type(l, v);
 
 	return switch(luaType) {
-
 		case Lua.LUA_TNIL:
 			null;
 
@@ -145,7 +141,6 @@ public static function fromLua(l:State, v:Int):Any {
 
 		case Lua.LUA_TTABLE:
 			toHaxeObj(l, v);
-
 		
 		case Lua.LUA_TFUNCTION:
             Lua.pushvalue(l, v);
@@ -159,11 +154,10 @@ public static function fromLua(l:State, v:Int):Any {
 }
 
 public static function toHaxeObj(l, i:Int):Any {
-
 	var hasItems = false;
 	var array = true;
 
-	loopTable(l, i,{
+	loopTable(l, i, {
 		hasItems = true;
 
 		if(Lua.type(l, -2) != Lua.LUA_TNUMBER)
@@ -173,47 +167,37 @@ public static function toHaxeObj(l, i:Int):Any {
 
 		if(index < 0 || Std.int(index) != index)
 			array = false;
-
 	});
 
 	if(!hasItems) return {};
 
 	if(array){
-
 		final v:Array<Dynamic> = [];
 
-		loopTable(l, i,{
+		loopTable(l, i, {
 			v[Std.int(Lua.tonumber(l, -2)) - 1] = fromLua(l, -1);
 		});
 
 		return cast v;
-
 	}
 
 	final v:DynamicAccess<Any> = {};
 
-	loopTable(l, i,{
-
-		switch Lua.type(l, -2){
-
+	loopTable(l, i, {
+		switch Lua.type(l, -2) {
 			case t if(t == Lua.LUA_TSTRING):
-
 				var key = Lua.tostring(l, -2);
 				if(key != null) v.set(key, fromLua(l, -1));
 
 			case t if(t == Lua.LUA_TNUMBER):
-
 				v.set(Std.string(Lua.tonumber(l, -2)), fromLua(l, -1));
-
 		}
-
 	});
 
 	return v;
 }
 
 public static function callLuaFunction(l, ?func:String, ?args:Array<Dynamic> = null, ?multipleReturns:Bool=false):Dynamic {
-
 	if(func != null)
 		Lua.getglobal(l, func);
 
@@ -235,7 +219,6 @@ public static function callLuaFunction(l, ?func:String, ?args:Array<Dynamic> = n
 }
 
 public static function callLuaFuncNoReturns(l, func:String, ?args:Array<Dynamic> = null):Void {
-
 	Lua.getglobal(l, func);
 
 	if(args != null)
